9a70aad0b824c6ded1831a43837f3a9149ca9429
aly raafat <alewaraafat@gmail.com>
2023-02-16 22:28:50 +0200

    refactoring the code and post launches

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-const planetsRouter = require('./routes/planets/planets.router');
 const path = require('path');
 const app = express();
+const planetsRouter = require('./routes/planets/planets.router');
+const launchesRouter = require('./routes/launches/launches.router');
 
 app.use(cors({
     origin: 'http://localhost:3000'
@@ -11,8 +12,9 @@ app.use(cors({
 app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname,'..','public')))
-app.use(planetsRouter);
-app.get('/', (req,res)=>{
+app.use('/planets',planetsRouter);
+app.use('/launches',launchesRouter);
+app.get('/*', (req,res)=>{
     res.sendFile(path.join(__dirname,'..','public','index.html'))
 })
 
---
+++ server/src/models/launches.model.js
@@ -0,0 +1,36 @@
+const launches = new Map();
+
+let latestFlightNumber = 100;
+const launch = {
+    flightNumber:100,
+    mission:"Kepler Exploration X",
+    rocket:"Explorer IS1",
+    launchDate: new Date("December 27,2030"),
+    destination: "Kepler-442 b",
+    customer: ["ZTM","NASA"],
+    upcoming:true,
+    success: true
+}
+
+launches.set(launch.flightNumber, launch);
+
+function getAllLaunches() {
+    return Array.from(launches.values());
+}
+
+function addNewLaunch(launch) {
+    latestFlightNumber ++;
+    launches.set(
+        latestFlightNumber, 
+        Object.assign(launch, {
+            flightNumber:latestFlightNumber,
+            customers: ['ZTM', 'NASA'],
+            upcoming:true,
+            success:true
+        })
+    );
+}
+module.exports = {
+    getAllLaunches,
+    addNewLaunch
+}
