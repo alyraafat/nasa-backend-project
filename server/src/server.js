f4e8f95c7ecf8ec8976df7fe7eccd3fd17bd7b1f
aly raafat <alewaraafat@gmail.com>
2023-02-18 22:44:41 +0200

    making mongoose schemas for planets and launches

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,33 @@
 const http = require('http');
 const app = require('./app');
-
+const mongoose = require('mongoose');
 const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = "mongodb+srv://nasa-api:IoBOENKdAFKbbH8P@nasacluster.ttq0mdv.mongodb.net/?retryWrites=true&w=majority"
 const server = http.createServer(app);
 
+mongoose.connection.once('open', ()=>{
+    console.log('MongoDB connection ready')
+});
+
+mongoose.connection.on('error', (err)=>{
+    console.error(err);
+});
+
+mongoose.set('strictQuery', false);
+
 async function startServer(){
+    await mongoose.connect(MONGO_URL
+    //     , {
+    //     useNewUrlParser: true,
+    //     // useFindAndModify : false,
+    //     // useCreateIndex: true,
+    //     useUnifiedTopology: true
+    // }
+    );
+
     await loadPlanetsData();
     
     server.listen(PORT, ()=>{
